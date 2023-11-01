1e4451855b70426d93a18fe6ef8862bf64d25b8a
Madu <137858554+MaduOfficial@users.noreply.github.com>
2023-10-31 21:25:58 -0300

    Add files via upload

---
+++ TesteCarro.java
@@ -0,0 +1,12 @@
+package com.madu.poo.palavra.chave.This;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro van = new Carro("Fiat", "Ducato");
+		
+		System.out.println(van.numPassageiros);
+	}
+
+}
